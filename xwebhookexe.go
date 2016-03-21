bfcb64349a8928e5a886eb668302db957a155230
xmisao <mail@xmisao.com>
2016-03-21 18:54:35 +0900

    add command execute function

--- xwebhookexe.go
+++ xwebhookexe.go
@@ -7,11 +7,21 @@ import (
     "strings"
     "net/http"
     "log"
+    "os"
+    "os/exec"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    w.WriteHeader(http.StatusOK)
-    fmt.Fprintf(w, "OK")
+func CreateExeHandler(exe string) func(http.ResponseWriter, *http.Request) {
+    return (func(w http.ResponseWriter, r *http.Request){
+      var err = exec.Command(os.Getenv("SHELL"), "-c", exe).Run()
+      if err == nil {
+        w.WriteHeader(http.StatusOK)
+        fmt.Fprintf(w, "Execute `%s` succeed.", exe)
+      } else {
+        w.WriteHeader(http.StatusInternalServerError)
+        fmt.Fprintf(w, "Execute `%s` failed. %s.", exe, err.Error())
+      }
+    })
 }
 
 func Log(handler http.Handler) http.Handler {
@@ -27,14 +37,15 @@ func main() {
     var b = flag.String("b", "", "Bind address.")
     var p = flag.Int("p", 8080, "Listen port.")
     var u = flag.String("u", "/", "Handle URL.")
+    var e = flag.String("e", "true", "Execute command by $SHELL.")
     flag.Parse()
 
     var ps = strconv.Itoa(*p)
-    log.Printf("Starting web server at %s:%s handle url %s", *b, ps, *u)
+    log.Printf("Starting web server at %s:%s execute `%s` when handle path '%s'.", *b, ps, *e, *u)
 
-    http.HandleFunc(*u, handler)
-    e := http.ListenAndServe(*b + ":" + ps, Log(http.DefaultServeMux))
-    if e != nil {
-      log.Panic(e)
+    http.HandleFunc(*u, CreateExeHandler(*e))
+    err := http.ListenAndServe(*b + ":" + ps, Log(http.DefaultServeMux))
+    if err != nil {
+      log.Panic(err)
     }
 }
