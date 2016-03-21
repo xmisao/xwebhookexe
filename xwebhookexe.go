9c0b08c71a5efcadf44654338fc4d94295fccc55
xmisao <mail@xmisao.com>
2016-03-21 18:19:38 +0900

    logging whole

--- xwebhookexe.go
+++ xwebhookexe.go
@@ -10,11 +10,17 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    info := []string{r.RemoteAddr, r.Proto, r.Method, r.RequestURI}
-    log.Println(strings.Join(info, " "))
-
     w.WriteHeader(http.StatusOK)
     fmt.Fprintf(w, "OK")
+}
+
+func Log(handler http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+      info := []string{r.RemoteAddr, r.Proto, r.Method, r.RequestURI}
+      log.Println(strings.Join(info, " "))
+
+      handler.ServeHTTP(w, r)
+    })
 }
 
 func main() {
@@ -27,5 +33,8 @@ func main() {
     log.Printf("Starting web server at %s:%s handle url %s", *b, ps, *u)
 
     http.HandleFunc(*u, handler)
-    http.ListenAndServe(*b + ":" + ps, nil)
+    e := http.ListenAndServe(*b + ":" + ps, Log(http.DefaultServeMux))
+    if e != nil {
+      log.Panic(e)
+    }
 }
